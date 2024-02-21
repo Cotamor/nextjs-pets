89e27a7d0d1a49f8fb1fa39c71f2d12cfcb11611
Cotamor <cotamor@gmail.com>
2024-02-22 02:54:03 +0900

    Basic chat functionality with Pusher

--- lib/chat.jsx
+++ lib/chat.jsx
@@ -1,8 +1,29 @@
-import { useState, useEffect, useRef } from "react"
+import Pusher from 'pusher-js'
+import { useState, useEffect, useRef } from 'react'
 
 export default function CharacterData() {
   const [isChatOpen, setIsChatOpen] = useState(false)
   const [unreadCount, setUnreadCount] = useState(0)
+  const [socketId, setSocketId] = useState()
+  const [messageLog, setMessageLog] = useState([])
+  const [userMessage, setUserMessage] = useState('')
+
+  useEffect(() => {
+    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHERKEY, {
+      cluster: process.env.NEXT_PUBLIC_CLUSTER,
+    })
+    // Establish connection, get socket_id
+    pusher.connection.bind('connected', () => {
+      setSocketId(pusher.connection.socket_id)
+    })
+    // Private channe must be prefixed with private-
+    const channel = pusher.subscribe('private-petchat')
+    // Message event
+    channel.bind('message', (data) => {
+      console.log(data)
+      setMessageLog((prev) => [...prev, data])
+    })
+  }, [])
 
   function openChatClick() {
     setIsChatOpen(true)
@@ -13,10 +34,35 @@ export default function CharacterData() {
     setIsChatOpen(false)
   }
 
+  function handleChatSubmit(e) {
+    e.preventDefault()
+    fetch('/admin/send-chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: userMessage,
+        socket_id: socketId,
+      }),
+    })
+    setMessageLog((prev) => [
+      ...prev,
+      { selfMessage: true, message: userMessage.trim() },
+    ])
+    setUserMessage('')
+  }
+
+  function handleInputChange(e) {
+    setUserMessage(e.target.value.trim())
+  }
+
   return (
     <>
       <div className="open-chat" onClick={openChatClick}>
-        { unreadCount > 0 &&<span className="chat-unread-badge">{unreadCount}</span>}
+        {unreadCount > 0 && (
+          <span className="chat-unread-badge">{unreadCount}</span>
+        )}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="22"
@@ -28,7 +74,13 @@ export default function CharacterData() {
           <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z" />
         </svg>
       </div>
-      <div className={isChatOpen ? 'chat-container chat-container--visible' :'chat-container'}>
+      <div
+        className={
+          isChatOpen
+            ? 'chat-container chat-container--visible'
+            : 'chat-container'
+        }
+      >
         <div className="chat-title-bar">
           <h4>Staff Team Chat</h4>
           <svg
@@ -44,37 +96,24 @@ export default function CharacterData() {
           </svg>
         </div>
         <div className="chat-log">
-          <div className="chat-message">
-            <div className="chat-message-inner">Hello world, kota.</div>
-          </div>
-
-          <div className="chat-message chat-message--self">
-            <div className="chat-message-inner">How do you do, mario.</div>
-          </div>
-          <div className="chat-message">
-            <div className="chat-message-inner">
-              We need to reach out for the latest contact for Purrsloud.
-            </div>
-          </div>
-          <div className="chat-message chat-message--self">
-            <div className="chat-message-inner">
-              Can you do that, mario? kdjkdjkdjfkdjkdfjkfjkjfkdfkjd
-            </div>
-          </div>
-          <div className="chat-message chat-message--self">
-            <div className="chat-message-inner">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis, molestiae cupiditate molestias maxime vitae, aliquam,
-              assumenda officia nemo fugit rerum quod nulla quibusdam obcaecati
-              repellendus! Quisquam, ipsa accusamus. Voluptas corporis iusto
-              sequi vel assumenda, maiores deserunt aliquam culpa unde,
-              voluptatum molestias suscipit tempora, quasi debitis. Repudiandae
-              culpa fugiat quod ut!
-            </div>
-          </div>
+          {messageLog.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className={
+                  item.selfMessage
+                    ? 'chat-message chat-message--self'
+                    : 'chat-message'
+                }
+              >
+                <div className="chat-message-inner">{item.message}</div>
+              </div>
+            )
+          })}
         </div>
-        <form>
+        <form onSubmit={handleChatSubmit}>
           <input
+            onChange={handleInputChange}
             type="text"
             autoComplete="off"
             placeholder="Your message here"
