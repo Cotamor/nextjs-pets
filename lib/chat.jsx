ad98416d878dd2aa34aa0074e0f8a288c43fd282
Cotamor <cotamor@gmail.com>
2024-02-21 15:40:32 +0900

    Open and close chat UI

--- lib/chat.jsx
+++ lib/chat.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect, useRef } from "react"
+
 export default function CharacterData() {
+  const [isChatOpen, setIsChatOpen] = useState(false)
+
+  function openChatClick() {
+    console.log('click')
+    setIsChatOpen(true)
+  }
+
+  function closeChatClick() {
+    console.log('close')
+    setIsChatOpen(false)
+  }
+
   return (
     <>
-      <div className="open-chat">
+      <div className="open-chat" onClick={openChatClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="22"
@@ -13,10 +27,11 @@ export default function CharacterData() {
           <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z" />
         </svg>
       </div>
-      <div className="chat-container">
+      <div className={isChatOpen ? 'chat-container chat-container--visible' :'chat-container'}>
         <div className="chat-title-bar">
           <h4>Staff Team Chat</h4>
           <svg
+            onClick={closeChatClick}
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
